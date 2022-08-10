a75ad0b9a4bc07564fab762c9bf9841d3457dcee
zrr <zrexrodriguez@gmail.com>
2022-08-10 14:56:34 -0600

    removed siblings from skillTreeNode

--- src/components/SkillTree/SkillTreeNode.js
+++ src/components/SkillTree/SkillTreeNode.js
@@ -1,13 +1,8 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-import { DataContext } from '../../contexts/DataContext'
-import { getParents } from '../../utils/quest'
-
 export default function SkillTreeNode ({ item }) {
-  const { state } = useContext(DataContext)
   const xp = item.skills.length * 10
-  const parents = getParents(state.data.levels, item)
 
   function NodeData ({ item, children }) {
     return (
@@ -23,14 +18,6 @@ export default function SkillTreeNode ({ item }) {
       <div className={`skillTreeNodeContainer key-${item.id}`} data-id={item.id}>
         <NodeData item={item}>
           <h4>XP: {xp}</h4>
-          <div className="skillTreeNode__siblings">
-            {parents?.length > 0 &&
-              <>
-              <h3>Previous Skill:</h3>
-              {parents.map(parent => <NodeData key={parent.id} item={parent} />)}
-              </>
-            }
-          </div>
         </NodeData>
       </div>
   )
