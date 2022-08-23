0fc5d293880227f210d934e89e5fcf81dd94f14d
zrr <zrexrodriguez@gmail.com>
2022-08-22 22:53:08 -0600

    refactor: removed ModalSkillEdit

--- src/components/Modal/ModalTypes.js
+++ src/components/Modal/ModalTypes.js
@@ -3,6 +3,5 @@ import ModalLevelEdit from './types/ModalLevelEdit'
 import ModalQuestAdd from './types/ModalQuestAdd'
 import ModalQuestEdit from './types/ModalQuestEdit'
 import ModalSkillAdd from './types/ModalSkillAdd'
-import ModalSkillEdit from './types/ModalSkillEdit'
 
-export { ModalLevelAdd, ModalLevelEdit, ModalQuestAdd, ModalQuestEdit, ModalSkillAdd, ModalSkillEdit }
+export { ModalLevelAdd, ModalLevelEdit, ModalQuestAdd, ModalQuestEdit, ModalSkillAdd }
--- src/components/Modal/types/ModalSkillEdit.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ModalSkillEdit () {
-  return (
-    <div>Hello World</div>
-  )
-}
