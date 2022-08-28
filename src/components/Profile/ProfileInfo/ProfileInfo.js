4e2ba006045bd895ad56a8cfa1fc6b923cf0cb32
zrr <zrexrodriguez@gmail.com>
2022-08-28 13:33:34 -0600

    feat: profile page

--- src/components/Profile/ProfileInfo/ProfileInfo.js
+++ src/components/Profile/ProfileInfo/ProfileInfo.js
@@ -2,29 +2,35 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export default function ProfileInfo ({ data }) {
+  function formattedDate (rawDate) {
+    const date = new Date(rawDate)
+    return `${date.getMonth()}/${date.getDay()}/${date.getFullYear()}`
+  }
+
+  function profiledata () {
+    const exclude = ['img', 'level', 'xp', 'gold', 'description']
+    return Object.entries(data)
+      .map(i => { return { key: i[0], value: i[1] } })
+      .filter(i => !exclude.includes(i.key))
+      .map(j => <li className='profileInfo__listItem' key={j.key}><em>{j.key[0].toUpperCase() + j.key.substring(1)}</em> : {j.key === 'birthday' ? formattedDate(j.value) : j.value}</li>)
+  }
+
   return (
     <div className='profileInfo'>
-      <div className='m-flex'>
+      <div className='m-flex profileInfo__intro'>
         <div>
           <h2 className='profileInfo__title'>{`${data.name} ${data.epiphet}`}</h2>
           <h3 className='profileInfo__subtitle'>{`(Lv. ${data.level} ${data.type} ${data.job})`}</h3>
         </div>
         <div>
-          <button className='m-button' type="button">Edit Profile</button>
+          <button className='profileInfo__button m-button ' type="button">Edit Profile</button>
         </div>
       </div>
-      <div className="profileInfo__data">
+      <div className="profileInfo__data m-flex">
         <ul className='profileInfo__list'>
-          <li className='profileInfo__listItem'>{data.name}</li>
-          <li className='profileInfo__listItem'>{data.epiphet}</li>
-          <li className='profileInfo__listItem'>{data.type}</li>
-          <li className='profileInfo__listItem'>{data.job}</li>
-          <li className='profileInfo__listItem'>{data.hometown}</li>
-          <li className='profileInfo__listItem'>{data.birthday}</li>
-          <li className='profileInfo__listItem'>{data.color}</li>
-          <li className='profileInfo__listItem'>{data.food}</li>
+          {profiledata()}
         </ul>
-        <p className="profileInfo__listItem profileInfo__description">{data.description}</p>
+        <p className="profileInfo__listItem profileInfo__description"><em>Description:  </em>{data.description}</p>
       </div>
     </div>
   )
