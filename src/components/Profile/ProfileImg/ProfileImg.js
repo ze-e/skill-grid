beb9b7379445fb39d5906cc3ca8174db9cd8c0ea
zrr <zrexrodriguez@gmail.com>
2022-09-10 22:37:12 -0600

    feat: implement avatar component

--- src/components/Profile/ProfileImg/ProfileImg.js
+++ src/components/Profile/ProfileImg/ProfileImg.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import { useNavigate } from 'react-router-dom'
-import { UserContext } from '../contexts/UserContext'
+import { UserContext } from '../../../contexts/UserContext'
 
 import Avatar from '../../Avatar/Avatar'
 
@@ -10,11 +10,7 @@ export default function ProfileImg ({ img }) {
   const { user } = useContext(UserContext)
   return (
     <div className='profileImg'>
-      <div className='profileImg__img'>
         <Avatar avatar={user.avatar} />
-      </div>
-      {/* <div className='profileImg__img' style={{ backgroundImage: `url(${img.src})` }}></div> */}
-
       <button className='profileImg__edit m-button' type='button' onClick={() => { navigate('/editAvatar') }} >Edit Avatar</button>
     </div>
   )
