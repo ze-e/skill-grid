16cedab670cfaaf3434860335679792b90caadfa
zrr <zrexrodriguez@gmail.com>
2022-10-21 23:55:08 -0600

    feat: added art

--- src/data/gearData.js
+++ src/data/gearData.js
@@ -1,16 +1,68 @@
 import pants1 from '../assets/gear/Legs/pants-1.png'
 import pants2 from '../assets/gear/Legs/pants-2.png'
 import pants3 from '../assets/gear/Legs/pants-3.png'
+import legs1 from '../assets/gear/Legs/Legs01.png'
+import legs2 from '../assets/gear/Legs/Legs02.png'
+import legs3 from '../assets/gear/Legs/Legs03.png'
+import legs4 from '../assets/gear/Legs/Legs04.png'
 
 import shirt1 from '../assets/gear/Body/blueshirt.png'
 import shirt2 from '../assets/gear/Body/greenshirt.png'
 import shirt3 from '../assets/gear/Body/shirt.png'
+import body1 from '../assets/gear/Body/Body01.png'
+import body2 from '../assets/gear/Body/Body02.png'
+import body3 from '../assets/gear/Body/Body03.png'
+import body4 from '../assets/gear/Body/Body04.png'
+
+import feet1 from '../assets/gear/Feet/Feet01.png'
+import feet2 from '../assets/gear/Feet/Feet02.png'
+import feet3 from '../assets/gear/Feet/Feet03.png'
+
+import head1 from '../assets/gear/Head/Head01.png'
+import head2 from '../assets/gear/Head/Head02.png'
+import head3 from '../assets/gear/Head/Head03.png'
+import head4 from '../assets/gear/Head/Head04.png'
+import head5 from '../assets/gear/Head/Head05.png'
+
+import l_hand1 from '../assets/gear/HandL/HandL01.png'
+import l_hand2 from '../assets/gear/HandL/HandL02.png'
+import l_hand3 from '../assets/gear/HandL/HandL03.png'
+import l_hand4 from '../assets/gear/HandL/HandL04.png'
+
+import r_hand1 from '../assets/gear/HandR/HandR01.png'
+import r_hand2 from '../assets/gear/HandR/HandR02.png'
+import r_hand3 from '../assets/gear/HandR/HandR03.png'
+import r_hand4 from '../assets/gear/HandR/HandR04.png'
 
 export default {
   pants1,
   pants2,
   pants3,
+  legs1,
+  legs2,
+  legs3,
+  legs4,
   shirt1,
   shirt2,
-  shirt3
+  shirt3,
+  body1,
+  body2,
+  body3,
+  body4,
+  feet1,
+  feet2,
+  feet3,
+  head1,
+  head2,
+  head3,
+  head4,
+  head5,
+  l_hand1,
+  l_hand2,
+  l_hand3,
+  l_hand4,
+  r_hand1,
+  r_hand2,
+  r_hand3,
+  r_hand4,
 }
--- src/data/sampleData.js
+++ src/data/sampleData.js
@@ -125,7 +125,7 @@ export const userData = [
       legs: '',
       feet: ''
     },
-    inventory: ['60b1788e-1b34-419d-a970-89312ac10e0c', '60b1788e-1b34-419d-a970-89312ac10e0d', '60b1788e-1b34-419d-a970-89312ac10e0e']
+    inventory: []
   },
   {
     data: {
@@ -160,7 +160,7 @@ export const userData = [
       legs: '',
       feet: ''
     },
-    inventory: ['60b1788e-1b34-419d-a970-89312ac10e0f', '60b1788e-1b34-419d-a970-89312ac10e0g', '60b1788e-1b34-419d-a970-89312ac10e0h']
+    inventory: []
   }
 ]
 
@@ -213,6 +213,166 @@ export const avatarData = {
 }
 
 export const itemData = [
+
+  {
+    id: '60b2888e-1b11-419d-a970-89312ac10e7h',
+    name: 'shirt 1',
+    location: 'body',
+    src: gear.shirt1,
+    cost: 10
+  }, {
+    id: '60b2888e-1b22-419d-a970-89312dc10e1d',
+    name: 'shirt 2',
+    location: 'body',
+    src: gear.shirt2,
+    cost: 10
+  }, {
+    id: '60b2888e-1b33-419d-a970-89312ac10e1d',
+    name: 'shirt 3',
+    location: 'body',
+    src: gear.shirt3,
+    cost: 10
+  },
+  {
+    id: '3e677c10-a706-46a3-a8b0-bb85ea8779e1',
+    name: 'chain mail',
+    location: 'body',
+    src: gear.body1,
+    cost: 10
+  },
+  {
+    id: 'b22b7dd9-e234-4722-86bf-f7508e540072',
+    name: 'battle armor',
+    location: 'body',
+    src: gear.body2,
+    cost: 10
+  },
+  {
+    id: 'b1eab51e-1c6c-40fe-9469-9d2258db8251',
+    name: 'black robe',
+    location: 'body',
+    src: gear.body3,
+    cost: 10
+  },
+  {
+    id: 'd802239b-99a0-46f2-b2f7-9c1b2bfc90b7',
+    name: 'knight armor',
+    location: 'body',
+    src: gear.body4,
+    cost: 10
+  },
+  {
+    id: 'e87f214c-d967-40aa-a5dc-250609127502',
+    name: 'bunny slippers',
+    location: 'feet',
+    src: gear.feet1,
+    cost: 10
+  },
+  {
+    id: 'c4bfb726-1f93-4c27-9494-fe7a0b08860b',
+    name: 'rocket boots',
+    location: 'feet',
+    src: gear.feet2,
+    cost: 10
+  },
+  {
+    id: '072a5814-eaa8-4a6b-857a-cd75523bd0f8',
+    name: 'spikey boots',
+    location: 'feet',
+    src: gear.feet3,
+    cost: 10
+  },
+  {
+    id: '80525212-41e6-4a54-af4f-3cb344da9886',
+    name: 'sword',
+    location: 'LHand',
+    src: gear.l_hand1,
+    cost: 10
+  },
+  {
+    id: '1eea31e1-7483-4bf8-88d5-35ffb3b442ea',
+    name: 'wizard staff',
+    location: 'LHand',
+    src: gear.l_hand2,
+    cost: 10
+  },
+  {
+    id: '29d4a077-8a2c-443f-8351-0536766519f3',
+    name: 'cyber glove',
+    location: 'LHand',
+    src: gear.l_hand3,
+    cost: 10
+  },
+  {
+    id: '1625d36e-af51-45da-8118-d731d1f12355',
+    name: 'lazer gun',
+    location: 'LHand',
+    src: gear.l_hand4,
+    cost: 10
+  },
+  {
+    id: '9b6825bf-db81-4fc6-b3ea-c1badbf27052',
+    name: 'shield',
+    location: 'RHand',
+    src: gear.r_hand1,
+    cost: 10
+  },
+  {
+    id: 'd8b0236b-8722-488f-a07f-6d76329c22d2',
+    name: 'battle axe',
+    location: 'RHand',
+    src: gear.r_hand2,
+    cost: 10
+  },
+  {
+    id: '7c9ce93a-e6e4-4b40-9d48-5d529cd4e580',
+    name: 'bow',
+    location: 'RHand',
+    src: gear.r_hand3,
+    cost: 10
+  },
+  {
+    id: '3d301693-1f5d-4108-8e0d-2c624901a9f0',
+    name: 'double edged sword',
+    location: 'RHand',
+    src: gear.r_hand4,
+    cost: 10
+  },
+  {
+    id: 'b263a355-38b2-4bcd-9f14-8a52d8d384c6',
+    name: 'knight helmet',
+    location: 'head',
+    src: gear.head1,
+    cost: 10
+  },
+  {
+    id: '03338dad-002f-41f5-9499-e5c5e55de59d',
+    name: 'magic hat',
+    location: 'head',
+    src: gear.head2,
+    cost: 10
+  },
+  {
+    id: 'ba4389ca-7773-45fd-9089-e926f16812b7',
+    name: 'mohawk',
+    location: 'head',
+    src: gear.head3,
+    cost: 10
+  },
+  {
+    id: 'ed09900f-b8cd-434c-bdc0-14733cd3c1c8',
+    name: 'fancy hat',
+    location: 'head',
+    src: gear.head4,
+    cost: 10
+  },
+  {
+    id: '7899d59b-a875-4b64-9834-860643502653',
+    name: 'vr goggles',
+    location: 'head',
+    src: gear.head5,
+    cost: 10
+  },
   {
     id: '60b1788e-1b77-419d-a970-89312ac10e1d',
     name: 'brown pants',
@@ -235,55 +395,31 @@ export const itemData = [
     cost: 10
   },
   {
-    id: '60b2888e-1b11-419d-a970-89312ac10e7h',
-    name: 'shirt 1',
-    location: 'body',
-    src: gear.shirt1,
-    cost: 10
-  }, {
-    id: '60b2888e-1b22-419d-a970-89312dc10e1d',
-    name: 'shirt 2',
-    location: 'body',
-    src: gear.shirt2,
-    cost: 10
-  }, {
-    id: '60b2888e-1b33-419d-a970-89312ac10e1d',
-    name: 'shirt 3',
-    location: 'body',
-    src: gear.shirt3,
+    id: '1765b578-80c5-4bc1-a978-6537308d4a13',
+    name: 'chainmail pants',
+    location: 'legs',
+    src: gear.legs1,
     cost: 10
   },
   {
-    id: '60b1788e-1b34-419d-a970-89312ac10e0c',
-    name: 'iron helm',
-    location: 'head',
+    id: 'be312d0e-2b36-415a-b337-19d238fd67fa',
+    name: 'space pants',
+    location: 'legs',
+    src: gear.legs2,
     cost: 10
   },
   {
-    id: '60b1788e-1b34-419d-a970-89312ac10e0d',
-    name: 'cyber glove',
-    location: 'LHand',
-    cost: 5
-  },
-  {
-    id: '60b1788e-1b34-419d-a970-89312ac10e0f',
-    name: 'staff',
-    location: 'LHand',
-    src: gear.staff1,
+    id: '74bb4cb3-c093-4b7d-bf6e-c8c4cdc3ff57',
+    name: 'leather pants',
+    location: 'legs',
+    src: gear.legs3,
     cost: 10
   },
   {
-    id: '60b1788e-1b34-419d-a970-89312ac10e0g',
-    name: 'sword',
-    location: 'LHand',
-    src: gear.sword1,
+    id: '1fe60c22-5d96-4f1a-9624-d9eaa05bc921',
+    name: 'striped pants',
+    location: 'legs',
+    src: gear.legs4,
     cost: 10
-  },
-  {
-    id: '60b1788e-1b34-419d-a970-89312ac10e0h',
-    name: 'shield',
-    location: 'RHand',
-    src: gear.shield1,
-    cost: 15
   }
 ]
