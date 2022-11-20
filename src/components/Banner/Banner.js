5e40b2f62dfae510b74109cb0546108606ce6ec2
ze-e <white.noise616@gmail.com>
2022-11-20 13:44:34 -0700

    added link to banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import LoginButton from '../LoginButton/LoginButton'
-
+import { Link } from 'react-router-dom'
 export default function Banner () {
   return (
     <div className='banner'>
-      <div className='banner__text'>
-        CodeQuest
-      </div>
+      <Link className='banner__text' to="/">CodeQuest</Link>
       <div className='banner__loginButton'>
         <LoginButton />
       </div>
