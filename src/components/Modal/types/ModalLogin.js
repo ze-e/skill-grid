0d8b3eb32bac2b3b7ccc05b58e4a055457569052
ze-e <white.noise616@gmail.com>
2022-11-19 19:00:28 -0700

    fix modals

--- src/components/Modal/types/ModalLogin.js
+++ src/components/Modal/types/ModalLogin.js
@@ -6,9 +6,14 @@ export default function ModalLogin ({ handleSubmit }) {
 
   return (
     <form onSubmit={handleSubmit} onChange={e => { setIsValid(e.target.value) }} >
-      <input placeholder="Enter username" minLength={3} maxLength={15} />
-      <input placeholder="Enter password" minLength={3} maxLength={15}/>
-      <button className="m-skillListButton button" type='submit' disabled={!isValid}>
+      <div className='m-flexColumnCenter'>
+        <h2 className='m-title-stroke-black' htmlFor="password">Log In</h2>
+        {/* <label className='m-title-stroke-black' htmlFor="name">Username</label> */}
+        <input name="name" placeholder="Enter username" minLength={3} maxLength={15} />
+        {/* <label className='m-title-stroke-black' htmlFor="password">Password</label> */}
+        <input name="password" placeholder="Enter password" minLength={3} maxLength={15}/>
+      </div>
+      <button className="m-modalButton" type='submit' disabled={!isValid}>
         Log In
       </button>
     </form>
